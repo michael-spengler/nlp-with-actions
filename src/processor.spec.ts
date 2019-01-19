34a8dd76b8aaf019a0e2e422044c3b3c4a25368d
Michael Spengler <michael.spengler@sap.com>
2019-01-19 13:19:43 +0100

    improved readability of unit tests

--- src/processor.spec.ts
+++ src/processor.spec.ts
@@ -5,9 +5,9 @@ let processor: Processor
 describe("Processor", () => {
     beforeEach(async () => {
         processor = new Processor()
-        await processor.learn(exampleMap)
     })
     it("processes direct match", async () => {
+        await processor.learn(exampleMap)
         expect(await processor.process("hi"))
             .toEqual({
                 actions: [],
@@ -16,6 +16,7 @@ describe("Processor", () => {
     })
 
     it("processes advanced shit", async () => {
+        await processor.learn(exampleMap)
         expect(await processor.process("how are you"))
             .toEqual({
                 actions: [],
